c4593d65d32113f0e1941fba2ffbc9a45d8f9a51
Rafael fischer <reinapratalwb@gmail.com>
2024-03-30 02:32:53 -0300

    uh

--- data/states/PlayState.hx
+++
@@ -1,8 +0,0 @@
-function beatHit(curBeat:Int)
-	{
-		if (camZoomingInterval < 1) camZoomingInterval = 1;
-		if (Options.camZoomOnBeat && camZooming && FlxG.camera.zoom < maxCamZoom && curBeat % camZoomingInterval == 0)
-		{
-			camHUD.zoom += 0;
-		}
-	}
